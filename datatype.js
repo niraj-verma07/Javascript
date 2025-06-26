1d83796f628412d5145ff89833a33e8f02fe2e29
Niraj Kumar Verma <vermaniraj9607@gmail.com>
2025-06-26 07:05:36 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,22 @@
+//JavaScript is a dynamically types language
+
+// Primitve Dataypes
+/* 7 types
+String
+Number
+Boolean
+null
+Undefined
+Symbol
+BigInt
+*/
+
+//Reference Type or Non - primitve
+
+/*
+Array
+Objects
+Functions
+ */
+const id = Symbol("12365")
+console.log(id)
